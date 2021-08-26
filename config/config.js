f0173e3cf92246304003cc21e4464942f0f26bca
Glitch (nebula-viz) <none>
2021-08-26 19:36:25 +0000

    ⚾️💵 Updated with Glitch

---
+++ config/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  qlikWebIntegrationId: "<ID>",
+  tenantDomain: "incognito.us.qlikcloud.com",
+  appId: "86396298-c6de-457b-8a98-c7a6e36f63db",
+  issuer: "incognito.us.qlikcloud.com",
+  keyid: "<ID>"
+};
